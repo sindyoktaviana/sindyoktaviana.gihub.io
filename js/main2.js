edb9b4d90bf97d54a5b1db0632122e7eebf6a0b0
sindyoktaviana <69460769+sindyoktaviana@users.noreply.github.com>
2020-08-10 19:25:08 +0700

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -3,9 +3,9 @@ var myImage = document.querySelector('img');
 myImage.onclick = function(){
 	var mySrc = myImage.getAttribute('src');
 	if(mySrc === 'imge/gambar3.jpg'){
-		myImage.setAttribute ('src','imge/gambar5.jpg');
+		myImage.setAttribute ('src','imge/gambar5.JPG');
 	}else{
-		myImage.setAttribute ('src','imge/gambar3.jpg');
+		myImage.setAttribute ('src','imge/gambar3.JPG');
 	}
 }
 
